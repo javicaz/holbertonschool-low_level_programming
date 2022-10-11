860867d825f5c83ad0ad7309bbfe37954498982a
Javi Icaza <javiicaza@gmail.com>
2022-10-10 20:22:26 -0500

    Readme and second submit

---
+++ functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+Functions Nesteed Loops
+
+Planing to link some functions to the ASCII language
+
+
+ASCII Link https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+/*task 0*/
+int _putchar(char);
+/*task 1*/ 
+void print_alphabet(void);
+/*task 2*/
+void print_alphabet_x10(void);
+/*task 3*/
+int _islower(int c);
+/*task 4*/
+int _isalpha(int c);
+/*task 5*/
+int print_sign(int n);
+/*task 6*/
+int _abs(int n);
+/*task 7*/
+int print_last_digit(int d);
+/*task 8*/
+void jack_bauer(void);
+/*task 9*/
+void times_table(void);
+/*task 10*/
+int add(int a, int b);
+/*task 11*/
+void print_to_98(int n);
