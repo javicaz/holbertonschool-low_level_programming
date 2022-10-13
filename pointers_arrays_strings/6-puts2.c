3c571df5c0a9809820b43556300d22c1166a0483
Javi Icaza <javiicaza@gmail.com>
2022-10-12 20:30:51 -0500

    Another solution same problem

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a  string to stdout
- * @str: string to print
- *
- * Return: void
+ * puts2 - print only even indexed chars from arg
+ * @str: string to print from
  */
+
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	_putchar('\n');
 }
