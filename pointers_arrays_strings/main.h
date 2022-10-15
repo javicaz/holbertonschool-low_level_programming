88df6a4d3f8a5d39879b9dc108ae385e1fb857b1
Javi Icaza <javiicaza@gmail.com>
2022-10-14 20:52:18 -0500

    memcpy

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -38,4 +38,5 @@ char *cap_string(char *);
 char *leet(char *s);
 /*Task 0 Memset*/
 char *_memset(char *s, char b, unsigned int n);
-
+/*Task 1 Memcpy*/
+char *_memcpy(char *dest, char *src, unsigned int n);
