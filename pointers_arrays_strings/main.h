28a1f519def41cdf805287ded7e8f704119dc0b0
Javi Icaza <javiicaza@gmail.com>
2022-10-14 22:20:12 -0500

    Finding a needle in a Haystack

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -46,3 +46,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 /*Task 4 Searchinset*/
 char *_strpbrk(char *s, char *accept);
+/*Task 5 Substracting*/
+char *_strstr(char *haystack, char *needle);
