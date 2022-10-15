82873954a4a1e8bbef7a661ca44b2a752e76cf3e
Javi Icaza <javiicaza@gmail.com>
2022-10-14 21:21:31 -0500

    Strspn

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -42,3 +42,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 /*Task 2 Memsearch*/
 char *_strchr(char *s, char c);
+/*Task 3 Mem Searchseption*/
+unsigned int _strspn(char *s, char *accept);
