ccef762cf391708672d0f3cae2acc8fcaf5a8219
Javi Icaza <javiicaza@gmail.com>
2022-10-19 22:46:24 -0500

    Prime changes

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,31 +1,28 @@
 #include "main.h"
-
+int prime_prev(int x, int y);
 /**
- * Description: prime_prev - checks to see if number is prime
- * @x: factor check
- * @y: possible prime number
- *
- * Return: 1 if prime, 0 if not
- */
-int prime_prev(int x, int y)
-{
-	if (x < 2 || y % x == 0)
-		return (0);
-	else if (x > y / 2)
-		return (1);
-	else
-		return (prime_prev(x + 1, y));
-}
-
-/**
- * Description: is_prime_number - states if number is prime
- * @n: number to check
- *
- * Return: 1 if prime, 0 if not
+ * Description: is_prime_number - says if an integer is a prime number or not
+ * prime_prev calculates prime numbers
+ * @n: number to evaluate
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	if (n == 2)
+	if (n <= 1)
+		return (0);
+	return (prime_prev(n, 2));
+}
+/**
+ * Description: prime_ prev - calculates if a number is prime with function
+ * @x: number to evaluate
+ * @y: iterator
+ * Return: 1 if n is prime number, 0 if not
+ */
+int prime_prev(int x, int y)
+{
+	if (x % y == 0 && x == y)
 		return (1);
-	return (prime_prev(2, n));
+	if (x % y == 0 && x != y)
+		return (0);
+	return (prime_prev(x, y + 1));
 }
