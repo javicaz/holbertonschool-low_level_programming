edf069fa7ff06a0269308dff8ad56ac9ae525d0c
Javi Icaza <javiicaza@gmail.com>
2022-10-25 00:01:51 -0500

    Malloc 0 Test

---
+++ malloc_free/README.md
@@ -0,0 +1,3 @@
+Malloc & Free
+
+In the workspace the _putchar and main.h archives must be located. 
---
+++ malloc_free/main.h
@@ -0,0 +1,4 @@
+/*_putchar*/
+int _putchar(char c);
+/*Task 1*/
+char *create_array(unsigned int size, char c);
