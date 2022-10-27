ef31dbf7c99b68ce55553cdc91c3a8b64deec004
Javi Icaza <javiicaza@gmail.com>
2022-10-26 20:02:20 -0500

    concat 2 strings

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,8 @@
 /*_putchar*/
 int _putchar(char c);
-/*Task 1*/
+/*Task 0*/
 char *create_array(unsigned int size, char c);
-/*Task 2*/
+/*Task 1*/
 char *_strdup(char *str);
+/*Task 2*/
+char *str_concat(char *s1, char *s2);
