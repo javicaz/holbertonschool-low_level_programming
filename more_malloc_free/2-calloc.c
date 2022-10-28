808594ff0a1e72d077227d7a826bc007dbb7fd3d
Javi Icaza <javiicaza@gmail.com>
2022-10-27 23:27:47 -0500

    Array with malloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*(a + i) = 0;
+	}
+	return (a);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -6,3 +6,5 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 /*Task 1 sterln*/
 int _strlen(char *s);
+/*Task 2 */
+void *_calloc(unsigned int nmemb, unsigned int size);
