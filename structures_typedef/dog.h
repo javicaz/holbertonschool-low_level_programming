8bee64531c2d357e8bf6e5b75bff07e81c23ead4
Javi Icaza <javiicaza@gmail.com>
2022-11-01 21:30:46 -0600

    Dog Char

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Short description
+ *@name : char name
+ *@age : float age in number
+ *@owner : char name of owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
