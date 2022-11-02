f8fd08f62b8ea060bd15bc3e9f357c9dc82f6045
Javi Icaza <javiicaza@gmail.com>
2022-11-01 22:28:54 -0600

    Dog commit correction

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	else
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
