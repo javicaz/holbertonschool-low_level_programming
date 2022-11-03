9f2af17c385b33b2998ab2c201307f67f670ad6b
Javi Icaza <javiicaza@gmail.com>
2022-11-02 23:09:59 -0600

    Like a rolling stone

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/* Putchar*/
+int _putchar(char c);
+/*Task 0 */
+void print_name(char *name, void(*f)(char *));
+
+#endif
