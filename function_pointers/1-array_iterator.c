025aca9cc1c9ebd69328424e84bf39a215d3acb4
Javi Icaza <javiicaza@gmail.com>
2022-11-02 23:30:15 -0600

    Pointer array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Execute function on each element
+ * @array: int to iterate over
+ * @size: size of the array
+ * @action: function to use on array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,5 +8,7 @@
 int _putchar(char c);
 /*Task 0 */
 void print_name(char *name, void(*f)(char *));
+/*Task 1*/
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
