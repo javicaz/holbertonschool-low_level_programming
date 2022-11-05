bd3a5cfc4f60b6eb23cc8c91ccb0a17104cb5268
Javi Icaza <javiicaza@gmail.com>
2022-11-04 22:43:06 -0600

    Print strings

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,5 +11,7 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 /*Task 1*/
 void print_numbers(const char *separator, const unsigned int n, ...);
+/*Task 2*/
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
