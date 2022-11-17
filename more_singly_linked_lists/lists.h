4277f590addd16d1048d68efeaae291781321de7
Javi Icaza <javiicaza@gmail.com>
2022-11-16 23:03:49 -0600

    add note

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -21,5 +21,7 @@ int _putchar(char c);
 size_t print_listint(const listint_t *h);
 /*Task 1*/
 size_t listint_len(const listint_t *h);
+/*Task 2*/
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
