c760f9fdb05d3fb3c1e1d6d501173a4d24dc0506
Javi Icaza <javiicaza@gmail.com>
2022-11-23 17:36:57 -0600

    print binary

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*_putchar*/
+int _putchar(char c);
+/*Task 0*/
 unsigned int binary_to_uint(const char *b);
-
+/*Task 1*/
+void print_binary(unsigned long int n);
 #endif
