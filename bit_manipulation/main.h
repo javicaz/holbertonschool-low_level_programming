622b4754f11b80a68e7da82e9724b721cae38859
Javi Icaza <javiicaza@gmail.com>
2022-11-23 22:25:03 -0600

    Set bit

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -9,5 +9,7 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 /*Task 2*/
 int get_bit(unsigned long int n, unsigned int index);
+/*Task 3*/
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
