f54cb13cfbd636302d548ff1486ddc762514aa5f
Javi Icaza <javiicaza@gmail.com>
2022-11-24 23:09:04 -0600

    Text Content

--- file_io/main.h
+++ file_io/main.h
@@ -9,4 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 /*Task 1*/
 int create_file(const char *filename, char *text_content);
+/*Task 2*/
+int append_text_to_file(const char *filename, char *text_content);
 #endif
