385889c96dccce15ef4d453c90b679ba7b574b8b
Javi Icaza <javiicaza@gmail.com>
2022-11-25 20:23:33 -0600

    Node added

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -24,5 +24,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 /*Task 1*/
 size_t dlistint_len(const dlistint_t *h);
+/*Task 2*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
