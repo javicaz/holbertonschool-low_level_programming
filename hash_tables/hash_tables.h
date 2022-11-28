0b93429f5e6aea18d896efa6fa9a0d4992ae9adc
Javi Icaza <javiicaza@gmail.com>
2022-11-28 17:27:34 -0600

    delete

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -48,5 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /*Task 5*/
 void hash_table_print(const hash_table_t *ht);
+/*Task 6*/
+void hash_table_delete(hash_table_t *ht);
 
 #endif
